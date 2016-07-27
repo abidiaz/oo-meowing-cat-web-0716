62cbc305f026bbbe6e39939a3f6b01c1c5cbbc5b
Abigail Diaz <github email address>
2016-07-27 00:50:42 -0400

    completed meowing cat lab

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,24 @@
-## code your solution here. 
+class Cat
+
+attr_accessor :cat_name
+
+
+# def new(cat_name)
+#   @name = cat_name
+# end
+
+def name=(new_cat_name)
+  @name = new_cat_name
+end
+
+def name
+  @name
+end
+
+
+
+def meow
+  puts "meow!"
+end
+
+end
